6c3af7f397829d525f359fe52d37335b2cda5a87
ezechiaskumbu77 <ezechiaskumbu77@gmail.com>
2021-02-27 13:17:29 +0100

    Setting-up the first EventListener resources: www.krycodes.atjayjo.com

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
 //jshint esversion:6
 
 // BUDGET CONTROLLER
-let budgetController = (function(){
+let budgetController = (function () {
     // some code
 })();
 
 
 //UI CONTROLLER
-let UIController = (function(){
+let UIController = (function () {
     // some code
 })();
 
 
 // GLOBAL APP CONTROLLER
-let controller = (function(budgetCtrl, UICtrl){
-// some code
-})(budgetController,UIController);
+let controller = (function (budgetCtrl, UICtrl) {
+    // Handling Event/click and keypress
+    let ctrlAddItem = function () {
+        //  1. Get the filed input data
+        //  2. Add the item to the budget controller
+        //  3. Add the item to the UI
+        //  4. Calculate the budget
+        //  15. Display the budget on the UI
+
+    };
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.addEventListener('keypress', function (e) {
+        if (e.keyCode === 13 || e.which === 13) {
+            ctrlAddItem();
+        }
+    });
+
+})(budgetController, UIController);
 
