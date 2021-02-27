cb3dfb1953a6e941c126c7d28b4f55e057d6edef
ezechiaskumbu77 <ezechiaskumbu77@gmail.com>
2021-02-27 12:09:01 +0100

    Initializatoin of my Project

---
+++ app.js
@@ -0,0 +1,19 @@
+//jshint esversion:6
+
+// BUDGET CONTROLLER
+let budgetController = (function(){
+    // some code
+})();
+
+
+//UI CONTROLLER
+let UIController = (function(){
+    // some code
+})();
+
+
+// GLOBAL APP CONTROLLER
+let controller = (function(budgetCtrl, UICtrl){
+// some code
+})(budgetController,UIController);
+
