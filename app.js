6d22d761a96370675830d8d96e5337cf2ac9e55f
ezechiaskumbu77 <ezechiaskumbu77@gmail.com>
2022-06-25 23:46:15 +0100

    Initialization function
    Creating an initialiation function

--- app.js
+++ app.js
@@ -36,27 +36,36 @@ let UIController = (function () {
 
 // GLOBAL APP CONTROLLER
 let controller = (function (budgetCtrl, UICtrl) {
-  let DOM = UICtrl.getDomStrings();
+  let setupEventListeners = function () {
+    let DOM = UICtrl.getDomStrings();
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function (e) {
+      if (e.keyCode == 13 || e.which == 13) {
+        ctrlAddItem();
+      }
+    })
+
+  }
   let ctrlAddItem = function () {
     // TodoLis
     // 1. Get the field input data
 
     let input = UICtrl.getinput()
-    console.log(input)
-
-
+  
     // 2. Add item to the budget controller
     // 3. Add item to the UI
     // 4. Calculate the budget
     // 5. Display the budget on the UI
     
   }
-  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-  document.addEventListener('keypress', function (e) {
-    if (e.keyCode == 13 || e.which == 13) {
-      ctrlAddItem();
+  return {
+    init: function(){
+      console.log("Application has started!")
+      setupEventListeners()
     }
-  })
+  }
 
 })(budgetController, UIController);
+
+controller.init()
