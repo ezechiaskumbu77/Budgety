f5c7aa71d413d1ecdbc1dd65931989b4c9dc2594
ezechiaskumbu77 <ezechiaskumbu77@gmail.com>
2022-07-24 00:38:23 +0100

    Creating Income and Expensive Function

--- app.js
+++ app.js
@@ -1,71 +1,84 @@
 //jshint esversion:6
-// Budget 
+// Budget
 
-// BUDGET CONTROLLER 
-const budgetController = (function () {
+// BUDGET CONTROLLER
+let budgetController = (function () {
+  let Expense = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
 
-}
-)();
+  let Income = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  let data = {
+    allItems: {
+      exp: [],
+      inc: [],
+    },
+
+    totals: {
+      exp: 0,
+      inc: 0
+    }
+  };
+})();
 
 //UI CONTROLLER
 let UIController = (function () {
   let DomStrings = {
-    inputType : '.addType',
-    inputDescription : '.addDescription',
-    inputValue : '.addValue',
-    inputBtn : '.addBtn',
-  }
-  return{
-    getinput:function(){
-
+    inputType: ".addType",
+    inputDescription: ".addDescription",
+    inputValue: ".addValue",
+    inputBtn: ".addBtn",
+  };
+  return {
+    getinput: function () {
       return {
-         type : document.querySelector(DomStrings.inputType).value, //will be either in or exp
-         description : document.querySelector(DomStrings.inputDescription).value,
-         value : document.querySelector(DomStrings.inputValue).value
+        type: document.querySelector(DomStrings.inputType).value, //will be either in or exp
+        description: document.querySelector(DomStrings.inputDescription).value,
+        value: document.querySelector(DomStrings.inputValue).value,
       };
-     
     },
-    getDomStrings:function(){
+    getDomStrings: function () {
       return DomStrings;
-    }
-
-  }
-
-}
-)();
+    },
+  };
+})();
 
 // GLOBAL APP CONTROLLER
 let controller = (function (budgetCtrl, UICtrl) {
   let setupEventListeners = function () {
     let DOM = UICtrl.getDomStrings();
-    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
 
-    document.addEventListener('keypress', function (e) {
+    document.addEventListener("keypress", function (e) {
       if (e.keyCode == 13 || e.which == 13) {
         ctrlAddItem();
       }
-    })
-
-  }
+    });
+  };
   let ctrlAddItem = function () {
     // TodoLis
     // 1. Get the field input data
 
-    let input = UICtrl.getinput()
-  
+    let input = UICtrl.getinput();
+
     // 2. Add item to the budget controller
     // 3. Add item to the UI
     // 4. Calculate the budget
     // 5. Display the budget on the UI
-    
-  }
+  };
   return {
-    init: function(){
-      console.log("Application has started!")
-      setupEventListeners()
-    }
-  }
-
+    init: function () {
+      console.log("Application has started!");
+      setupEventListeners();
+    },
+  };
 })(budgetController, UIController);
 
-controller.init()
+controller.init();
